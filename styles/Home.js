dbfc7c83fbd7da88e3241f79e1f3e62609f297e1
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-26 21:19:22 +0300

    Test style file

---
+++ styles/Home.js
@@ -0,0 +1,9 @@
+import styled, { css } from 'styled-components';
+
+export const H1 = styled.h1`
+    color: red !important;
+    
+    /* ${props => props.active && css`
+        display: block;
+    `} */
+` 
