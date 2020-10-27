748fd27354334e193dd009a55214b67fdbc96dd0
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-28 02:24:12 +0300

    Added a new package for interacting with images

--- next.config.js
+++ next.config.js
@@ -7,3 +7,6 @@ module.exports = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
+
+const withImages = require('next-images');
+module.exports = withImages();
