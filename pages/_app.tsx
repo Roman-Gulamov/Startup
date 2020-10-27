949961ae1b5d872e7e0af021ab08e7622d17ee57
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-28 02:24:50 +0300

    Functions for styled-components to work correctly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,9 @@
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
+import { useEffect } from 'react';
+
+const MyApp = ({ Component, pageProps }) => { 
+    return ( 
+        <Component {...pageProps} /> 
+    )
+}
 
 export default MyApp;
