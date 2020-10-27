5149891669528b85bab3e73ac50c9e59261a42f1
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-28 02:26:40 +0300

    Extra tag removed

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,10 @@
 import { AppWrapper } from '../components/AppWrapper';
-import { H1 } from '../styles/Home';
+
 
 const About = () => {
     return (
         <AppWrapper>
-            <H1>About!</H1>
+            <p>About!</p>
         </AppWrapper>
     )
 }
