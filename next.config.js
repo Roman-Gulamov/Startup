a7e503971177b5eb0151b45aabf7991ea76744fc
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-28 03:33:48 +0300

    Removed unnecessary prefixes

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const withImages = require('next-images');
 
 module.exports = withImages({
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
     dynamicAssetPrefix: true,
     webpack(config, options) {
         return config
     },
-
+    
     env: {
         API_URL: process.env.API_URL
     },
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 })
