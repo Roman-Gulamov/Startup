6e121549c4a1f140261de6f1e337f046542491ec
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-28 03:25:54 +0300

    Removed unnecessary prefixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,13 @@ import Link from 'next/link';
 import { AppWrapper } from '../components/AppWrapper';
 import { Wrapper, HomeTitle, HomeSubtitle, HomeButton } from '../styles/Home';
 
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
 const Index = () => {
     return (
         <AppWrapper>
             <Wrapper>
                 <HomeTitle>Welcome to startup</HomeTitle>
                 <HomeSubtitle>Your favourite creative agency template</HomeSubtitle>
-                <Link href='/contact' as={`${prefix}/contact`}><HomeButton>Get started</HomeButton></Link>
+                <Link href='/contact'><HomeButton>Get started</HomeButton></Link>
             </Wrapper>
         </AppWrapper>
     )
