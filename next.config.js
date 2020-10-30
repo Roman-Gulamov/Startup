3da2274f01e454d1a51804d7244511cafca621db
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-30 03:54:36 +0300

    Updated config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const withImages = require('next-images');
 
 module.exports = withImages({
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
     dynamicAssetPrefix: true,
     webpack(config, options) {
         return config
@@ -11,5 +11,11 @@ module.exports = withImages({
     env: {
         API_URL: process.env.API_URL
     },
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    images: {
+        deviceSizes: [320, 420, 768, 1024, 1200],
+        iconSizes: [],
+        loader: "cloudinary",
+        path: "https://res.cloudinary.com/romangulamov/image/upload",
+    },
 })
