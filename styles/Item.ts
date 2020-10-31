6f04398bf5bc140dd7d3801a3e64ed13dd5600f6
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-10-31 18:35:49 +0300

    Universal Styles

---
+++ styles/Item.ts
@@ -0,0 +1,58 @@
+import styled, { css } from 'styled-components';
+import { IPropsItemImg, IPropsItemInvisible } from '../interfaces/styledInterface';
+
+
+export const ItemImage = styled.div<IPropsItemImg>`
+    position: relative;
+    width: ${props => props.width};
+    height: auto;
+
+    ${props => props.services && css`
+        @media screen and (max-width: 768px) {
+            width: 6em;
+        }
+    `}
+
+    & img {
+        width: 100%;
+        height: auto;
+    }
+`
+
+export const ItemInvisible = styled.div<IPropsItemInvisible>`
+    position: absolute;
+    top: 0%;
+    left: 0%;
+    width: 100%;
+    height: ${props => props.height};
+    display: flex;
+    padding: 0 3em 2em;
+    background-color: ${props => props.background};
+    opacity: 0;
+    transition: opacity 0.4s ease;
+
+    &:hover {
+        opacity: 1;
+    }
+
+    ${props => props.aboutProps && css`
+        justify-content: space-around;
+        align-items: flex-end;
+
+        & a {
+        cursor: pointer;
+
+            & svg {
+                width: 1.3em !important;
+                height: 100%;
+            }
+        }
+    `}
+
+    ${props => props.worksProps && css`
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-weight: 600;
+    `}
+`
