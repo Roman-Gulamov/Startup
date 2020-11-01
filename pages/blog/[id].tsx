ca6fd584bb5242d73ffe1808fc5de91797a98f56
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-01 21:42:57 +0300

    Function change

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -88,25 +88,16 @@ const Blog = ({ blog: serverBlog } : BlogPostProps): JSX.Element => {
 export default Blog;
 
 
-// Blog.getInitialProps = async ({query, req}: BlogNextPageContext) => { 
-//     if (!req) { 
-//         return {blog: null}
-//     }
-//     const response = await fetch(`${process.env.API_URL}/blog/${query.id}`);
-//     const blog: IBlog = await response.json();
-
-//     return {
-//         blog
-//     }
-// }
-
-
-export const getServerSideProps = async ({ query, req }: BlogNextPageContext) => {
+Blog.getInitialProps = async ({query, req}: BlogNextPageContext) => { 
     if (!req) { 
         return {blog: null}
     }
     const response = await fetch(`${process.env.API_URL}/blog/${query.id}`);
     const blog: IBlog = await response.json();
 
-    return {props: {blog}}
+    return {
+        blog
+    }
 }
+
+
