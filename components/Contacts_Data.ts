d9acb8326174dc2bf12b2dc7686bf40dc4a6b227
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-03 03:25:59 +0300

    Added data for mapping

---
+++ components/Contacts_Data.ts
@@ -0,0 +1,74 @@
+import { faMapMarkerAlt, faMobileAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+export const CONTACTS_DATA = {
+    "templates": [
+        {
+            "id": 1,
+            "icon": faMapMarkerAlt,
+            "title": "Address",
+            "firstExample": "312, 7th Ave, New York",
+            "secondExample": "NY 101200, United States of America"
+        },
+        {
+            "id": 2,
+            "icon": faMobileAlt,
+            "title": "Hotline (24x7)",
+            "firstExample": "+65 0052 300, +65 88251",
+            "secondExample": "210 +88 01723 511 340"
+        },
+        {
+            "id": 3,
+            "icon": faEnvelope,
+            "title": "E-mail",
+            "firstExample": "shapedtheme@gmail.com",
+            "secondExample": "khalilkode@gmail.com"
+        }
+    ],
+    "inputs": [ 
+        {
+            "id": 1,
+            "name": "name",
+            "component": "input",
+            "autoFocus": true,
+            "autoComplete": "true",
+            "type": "text",
+            "placeholder": "Your name"
+        },
+        {
+            "id": 2,
+            "name": "email",
+            "component": "input",
+            "autoFocus": false,
+            "autoComplete": "true",
+            "type": "email",
+            "placeholder": "Your e-mail"
+        },
+        {
+            "id": 3,
+            "name": "subject",
+            "component": "input",
+            "autoFocus": false,
+            "autoComplete": "true",
+            "type": "text",
+            "placeholder": "Your subject"
+        },
+        {
+            "id": 4,
+            "name": "company",
+            "component": "input",
+            "autoFocus": false,
+            "autoComplete": "true",
+            "type": "text",
+            "placeholder": "Company name"
+        },
+        {
+            "id": 5,
+            "name": "message",
+            "component": "textarea",
+            "autoFocus": false,
+            "autoComplete": "null",
+            "type": null,
+            "placeholder": "Write your message"
+        }
+    ]
+}
